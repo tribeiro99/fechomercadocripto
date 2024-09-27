f2b58750f3a043daa8b474552f9a4c0bdd52927d
Tiago Ribeiro <161218492+tribeiro99@users.noreply.github.com>
2024-09-27 17:09:45 +0100

    Update ultimos3_valores_xmr_eth_btc.py

--- ultimos3_valores_xmr_eth_btc.py
+++ ultimos3_valores_xmr_eth_btc.py
@@ -1,25 +1,12 @@
 import vectorbt as vbt
 import pandas as pd 
 
-eth_price =vbt.YFData.download(
-        "ETH-EUR",
+ 
+cripto_price =vbt.YFData.download(
+        ["BTC-EUR","ETH-EUR","XMR.EUR"],
         missing_index='drop').get("Close")
 
-print(eth_price.tail())
- 
-btc_price =vbt.YFData.download(
-        "BTC-EUR",
-        missing_index='drop').get("Close")
-
-print(btc_price.tail())
+print(cripto_price.tail())
  
 
-
-
-xmr_price =vbt.YFData.download(
-        "XMR-EUR",
-        missing_index='drop').get("Close")
-
-print(xmr_price.tail())
- 
 
